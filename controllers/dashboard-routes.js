74df5ee305ffa69b3146e5753a3f86b8b8ecbae4
mcc <costanza+git@gmail.com>
2021-06-25 21:47:29 -0700

    add liked/disliked posts dashboard views

--- controllers/dashboard-routes.js
+++ controllers/dashboard-routes.js
@@ -1,9 +1,11 @@
 const router = require('express').Router();
 const sequelize = require('../config/connection');
-const { Post, User, Comment, Tag, PostTag } = require('../models');
+const { Post, User, Comment, Tag, PostTag, Vote } = require('../models');
 const { withAuth } = require('../utils/auth');
 
-router.get('/', (req, res) => {
+const POST_IMAGE_WIDTH = 400;
+
+router.get('/', withAuth, (req, res) => {
   Post.findAll({
     where: {
       user_id: req.session.user_id
@@ -33,17 +35,111 @@ router.get('/', (req, res) => {
       {
         model: User,
         attributes: [['id', 'post_author_id'], ['nickname', 'post_author']]
+      },
+      {
+        model: Vote,
+        attributes: [['id', 'vote_id'], 'like']
       }
     ]
   })
     .then(dbPostData => {
       const posts = dbPostData.map(post => post.get({ plain: true }));
-      res.render('dashboard', { posts, loggedIn: req.session.loggedIn });
+      for (let i = 0; i < posts.length; i++) {
+        posts[i].image_url_sized = posts[i].image_url ? posts[i].image_url.replace('upload/', 'upload/' + `c_scale,w_${POST_IMAGE_WIDTH}/`) : '';
+      }
+      res.render('dashboard-posts', { posts, loggedIn: req.session.loggedIn });
     })
     .catch(err => {
       console.log(err);
       res.status(500).json(err);
     });
+});
+
+const getVoted = function (req, type) {
+  return Vote.findAll({
+    where: {
+      user_id: req.session.user_id,
+      like: (type === 'likes')
+    },
+    attributes: [
+      'id'
+    ],
+    include: [
+      {
+        model: Post,
+        attributes: [
+          'id',
+          'title',
+          'description',
+          'image_url',
+          'latitude',
+          'longitude',
+          'created_at',
+          [sequelize.literal('(SELECT COUNT(*) FROM vote WHERE post.id = vote.post_id AND `like`)'), 'likes'],
+          [sequelize.literal('(SELECT COUNT(*) FROM vote WHERE post.id = vote.post_id AND NOT `like`)'), 'dislikes'],
+          [sequelize.literal('(SELECT COUNT(*) FROM comment WHERE post.id = comment.post_id)'), 'comment_count']
+        ],
+        order: [['post.created_at', 'DESC'], ['post.id', 'DESC']],
+        include: [
+          {
+            model: Tag,
+            attributes: [['id', 'tag_id'], 'tag_name'],
+            through: {
+              model: PostTag,
+              attributes: []
+            },
+          },
+          {
+            model: User,
+            attributes: [['id', 'post_author_id'], ['nickname', 'post_author']]
+          },
+          {
+            model: Vote,
+            attributes: [['id', 'vote_id'], 'like']
+          }
+        ]
+      }
+    ]
+  })
+}
+
+router.get('/likes', withAuth, (req, res) => {
+  getVoted(req, 'likes')
+    .then(dbPostData => {
+      const posts = dbPostData.map(like => like.get({ plain: true }).post);
+      for (let i = 0; i < posts.length; i++) {
+        posts[i].image_url_sized = posts[i].image_url ? posts[i].image_url.replace('upload/', 'upload/' + `c_scale,w_${POST_IMAGE_WIDTH}/`) : '';
+      }
+      res.render('dashboard-likes', { posts, tab: 'Liked', loggedIn: req.session.loggedIn });
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
+
+router.get('/dislikes', withAuth, (req, res) => {
+  getVoted(req, 'dislikes')
+    .then(dbPostData => {
+      const posts = dbPostData.map(like => like.get({ plain: true }).post);
+      for (let i = 0; i < posts.length; i++) {
+        posts[i].image_url_sized = posts[i].image_url ? posts[i].image_url.replace('upload/', 'upload/' + `c_scale,w_${POST_IMAGE_WIDTH}/`) : '';
+      }
+      res.render('dashboard-likes', { posts, tab: 'Disliked', loggedIn: req.session.loggedIn });
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
+
+router.get('/create', withAuth, (req, res) => {
+  const post = {
+    id: 0,
+    title: '',
+    body: '',
+  };
+  res.render('edit-post', { post, loggedIn: req.session.loggedIn });
 });
 
 router.get('/edit/:id', withAuth, (req, res) => {
@@ -95,15 +191,6 @@ router.get('/edit/:id', withAuth, (req, res) => {
       console.log(err);
       res.status(500).json(err);
     });
-});
-
-router.get('/create', withAuth, (req, res) => {
-  const post = {
-    id: 0,
-    title: '',
-    body: '',
-  };
-  res.render('edit-post', { post, loggedIn: req.session.loggedIn });
 });
 
 module.exports = router;
